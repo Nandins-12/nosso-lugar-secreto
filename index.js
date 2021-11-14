9e979320e4613f49ceff24320a830a814d937614
Nandins-12 <fernandogdias12@gmail.com>
2021-11-14 01:53:36 -0300

    Update Responsividade Menu, Update Fonte

---
+++ index.js
@@ -0,0 +1,18 @@
+const date = new Date()
+const birthdayDate = new Date(2021, 7, 11)
+const loveDate = new Date(2019, 5, 30)
+
+const mileSecondsBetweenDate = Math.abs(date.getTime() - birthdayDate.getTime())
+
+document.querySelector('#day-area').innerText = Math.ceil(mileSecondsBetweenDate / 86400000)
+
+document.querySelector('#love-area').innerText = date.getFullYear() - loveDate.getFullYear()
+
+function toggle() {
+    const nav = document.querySelector('header div.container nav')
+    nav.classList.toggle('active')
+    document.body.classList.toggle('menu-compatibility')
+    document.querySelector('#btn-mobile').classList.toggle('active')
+}
+
+const buttonMenu = document.querySelector('#btn-mobile')
